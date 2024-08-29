33ec494e1fb3ffc0d8eedcbf540e81a6fc36001a
Mono <misupriority@gmail.com>
2024-08-30 00:39:31 +0600

    Добавил отладку

--- src/main/java/com/helixo/Main.java
+++ src/main/java/com/helixo/Main.java
@@ -2,10 +2,13 @@ package com.helixo;
 
 import com.helixo.test.TestMain;
 
-import java.io.IOException;
-
 public class Main {
-    public static void main(String[] args) throws IOException {
-        TestMain.main(args);
+    public static void main(String[] args) {
+        try {
+            TestMain.main(args);
+        } catch (Exception e) {
+            System.out.println("Ошибка при выполнении программы: " + e.getMessage());
+            e.printStackTrace();
+        }
     }
 }
