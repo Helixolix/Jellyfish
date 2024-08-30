6e96252903b432e529839aec385596212bebfefd
Mono <misupriority@gmail.com>
2024-08-30 12:59:27 +0600

    Update run.bat

--- src/main/resources/run.bat
+++ src/main/resources/run.bat
@@ -2,17 +2,26 @@
 setlocal
 
 if "%~1"=="" (
-    echo Specify the path to the JAR file.
-    echo Usage: run "C:\Users\filip\Documents\DerlaryLenguage-master\target\DerlaryLenguage-1.0-SNAPSHOT.jar"
+    echo Specify the path to the .dil file.
+    echo Usage: run "C:\Users\filip\Documents\DerlaryLenguage-master\target\DerlaryLenguage-1.0-SNAPSHOT.jar" "example.dil"
     goto :eof
 )
 
+if "%~2"=="" (
+    echo Specify the path to the .dil file.
+    echo Usage: run "C:\Users\filip\Documents\DerlaryLenguage-master\target\DerlaryLenguage-1.0-SNAPSHOT.jar" "example.dil"
+    goto :eof
+)
 
 set "jarpath=%~1"
-
+set "dilpath=%~2"
 
 if exist "%jarpath%" (
-    java -jar "%jarpath%" %*
+    if exist "%dilpath%" (
+        java -jar "%jarpath%" "%dilpath%"
+    ) else (
+        echo The specified .dil file was not found: %dilpath%
+    )
 ) else (
     echo The specified JAR file was not found: %jarpath%
 )
