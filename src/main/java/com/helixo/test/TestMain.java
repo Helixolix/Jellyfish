8cd51b2e3372ffa139109d35a089fbbd1f04df1b
MonoSudji <misupriority@gmail.com>
2024-08-31 15:39:07 +0600

    Изменено расширение файла на .jfl

--- src/main/java/com/helixo/test/TestMain.java
+++ src/main/java/com/helixo/test/TestMain.java
@@ -16,21 +16,21 @@ public class TestMain {
 
     public static void main(String[] args) {
         try {
-            // загрузка файла .dil из ресов
-            String resourcePath = "/test.dil";
+
+            String resourcePath = "/test.jfl";
             InputStream inputStream = TestMain.class.getResourceAsStream(resourcePath);
 
             if (inputStream == null) {
                 System.out.println(ANSI_RED + "Ошибка: Ресурс не найден: " + resourcePath);
                 return;
             }
-            
+
             String input = new String(inputStream.readAllBytes(), StandardCharsets.UTF_8);
 
-            // токенизация содержимого файла
+
             List<Token> tokens = new Lexer(input).tokenize();
 
-            // паринг токенов и выполнение программы
+
             Statement program = new Parser(tokens).parse();
             program.execute();
 
--- src/main/resources/run.bat
+++ src/main/resources/run.bat
@@ -2,28 +2,21 @@
 setlocal
 
 if "%~1"=="" (
-    echo Specify the path to the .dil file.
-    echo Usage: run "C:\Users\Noutb\Downloads\DerlaryLenguage\src\main\resources\DerlaryLenguage-1.0-SNAPSHOT.jar" "example.dil"
+    echo Specify the path to the .jfl file.
+    echo Usage: jfl "example.jfl"
     goto :eof
 )
 
-if "%~2"=="" (
-    echo Specify the path to the .dil file.
-    echo Usage: run "C:\Users\Noutb\Downloads\DerlaryLenguage\src\main\resources\DerlaryLenguage-1.0-SNAPSHOT.jar" "example.dil"
-    goto :eof
-)
+set "jflpath=%~1"
 
-set "jarpath=%~1"
-set "dilpath=%~2"
-
-if exist "%jarpath%" (
-    if exist "%dilpath%" (
-        java -jar "%jarpath%" "%dilpath%"
+if exist "%jflpath%" (
+    if /i "%~x1"==".jfl" (
+        java -jar "%~dp0jfl.jar" "%jflpath%"
     ) else (
-        echo The specified .dil file was not found: %dilpath%
+        echo The specified file is not a .jfl file: %jflpath%
     )
 ) else (
-    echo The specified JAR file was not found: %jarpath%
+    echo The specified .jfl file was not found: %jflpath%
 )
 
 endlocal
