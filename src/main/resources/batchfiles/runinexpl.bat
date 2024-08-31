07cf78b3f2d13201f28cd80de79055660b12dc6d
MonoSudji <misupriority@gmail.com>
2024-09-01 00:03:11 +0600

    Уброно двоеточие

--- src/main/resources/batchfiles/runinexpl.bat
+++ src/main/resources/batchfiles/runinexpl.bat
@@ -1,10 +1,10 @@
 @echo off
 setlocal
 
-set "jarpath="C:\Program Files (x86)\JFL\DerlaryLenguage-1.0-SNAPSHOT.jar"
+set "jarpath=C:\Program Files (x86)\JFL\DerlaryLenguage-1.0-SNAPSHOT.jar"
 
 set "jflpath=%~1"
 
 java -jar "%jarpath%" "%jflpath%"
 
-pause
+endlocal
